5c5b94b931de4571d1f370709ed82be73b3cab38
sokrat <sergey.sipatov@gmail.com>
2015-06-16 16:25:29 +0300

    add search method

--- Geo.php
+++ Geo.php
@@ -4,6 +4,8 @@ namespace consultnn\api;
 
 class Geo extends AbstractDomain
 {
+    const TYPE_HOUSE = 'house';
+
     public function get($id)
     {
         $result =  $this->getSingle(
@@ -12,6 +14,26 @@ class Geo extends AbstractDomain
             [
                 'id' => $id,
                 'format' => 'json'
+            ]
+        );
+        return $result;
+    }
+
+    /**
+     * @param $query
+     * @param $types
+     * @return mappers\MapperInterface[]
+     * @throws exceptions\Exception
+     */
+    public function search($query, $types = [])
+    {
+        $result =  $this->getInternalList(
+            'dict/address',
+            [__NAMESPACE__.'\mappers\Geo', 'geoMapResolver'],
+            [
+                'name' => $query,
+                'format' => 'json',
+                'dictionaries' => $types
             ]
         );
         return $result;
