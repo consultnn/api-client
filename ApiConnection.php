331c910fa73dd94162c72786f75463c1bbeaeba9
Trubachev <trub.denchik@gmail.com>
2016-10-18 15:44:52 +0300

    DNS cache

--- ApiConnection.php
+++ ApiConnection.php
@@ -162,6 +162,7 @@ class ApiConnection
                 CURLOPT_FOLLOWLOCATION => false,
                 CURLOPT_USERAGENT => 'PHP ' . __CLASS__,
                 CURLOPT_ENCODING => 'gzip, deflate',
+                CURLOPT_DNS_USE_GLOBAL_CACHE => true
             ));
         }
         return $this->curl;
